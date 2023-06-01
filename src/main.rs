067e427aea6d2dcc916f7e053c3df1ff9b9be5f7
Alihan Tadal <tadal.alihan@gmail.com>
2023-06-01 18:35:41 +0300

    Add retrun and call.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 struct CPU {
     registers: [u8; 16],       // 16 adet 8 bitlik registerlar.
     position_in_memory: usize, //program count.
-    memory: [u8; 0x1000],      // 4096 byte memory.
+    memory: [u8; 0x1000],
+    stack: [u16; 16], // 4096 byte memory.
+    stack_pointer: usize,
 }
 
 impl CPU {
@@ -23,10 +25,14 @@ impl CPU {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = ((opcode & 0x000F) >> 0) as u8;
 
+            let nnn = opcode & 0xFFF;
+            // let kk = (opcode & 0x00FF) as u8
             match (c, x, y, d) {
                 (0, 0, 0, 0) => {
                     return; // 0x0000 olunca çalışmayı durdur.
                 }
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
@@ -46,6 +52,27 @@ impl CPU {
             self.registers[0xF] = 0;
         }
     }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow.");
+        }
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack overflow.");
+        }
+        self.stack_pointer -= 1;
+        let addr = self.stack[self.stack_pointer];
+        self.position_in_memory = addr as usize;
+    }
 }
 
 fn main() {
@@ -53,25 +80,29 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
-
-    // Register'lara değer ata. Programı yükle?
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     let mem = &mut cpu.memory;
-    mem[0] = 0x80;
-    mem[1] = 0x14; // opcode 8014 load et. register_1 + register_0
-    mem[2] = 0x80;
-    mem[3] = 0x24; // opcode 8024 load et. register_2 + register_0
-    mem[4] = 0x80;
-    mem[5] = 0x34; // opcode 8034 load et. register_3 + register_0
+    mem[0x000] = 0x21;
+    mem[0x001] = 0x00;
+    mem[0x002] = 0x21;
+    mem[0x003] = 0x00;
+    mem[0x004] = 0x00;
+    mem[0x005] = 0x00;
+
+    mem[0x100] = 0x80;
+    mem[0x101] = 0x14;
+    mem[0x102] = 0x80;
+    mem[0x103] = 0x14;
+    mem[0x104] = 0x00;
+    mem[0x105] = 0xEE;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
-
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    assert_eq!(cpu.registers[0], 45);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
