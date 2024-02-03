de20633d22f39bfc9a8357fe960233b6981c3b8e
alihantadal <tadal.alihan@gmail.com>
2024-02-03 23:08:36 +0300

    Add some opcode.

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,8 +1,11 @@
+use rand::random;
+
 pub const SCREEN_WIDTH: usize = 64;
 pub const SCREEN_HEIGHT: usize = 32;
 
 const RAM_SIZE: usize = 4096;
 const NUM_REGS: usize = 16;
+const NUM_KEYS: usize = 16;
 const FONTSET_SIZE: usize = 80;
 
 const FONTSET: [u8; FONTSET_SIZE] = [
@@ -31,7 +34,8 @@ pub struct Emu {
     v_regs: [u8; NUM_REGS],                       // V0 - VF
     i_reg: u16,                                   // I register
     sp: u16,                                      // Stack Pointer
-    stack: [u16; 16],                             // Stack
+    stack: [u16; 16],                             // 
+    keys: [bool; NUM_KEYS],
     dt: u8,                                       // Delay Timer
     st: u8,                                       // Sound Timer, emits a beep when it reaches 0
 }
@@ -44,6 +48,7 @@ impl Emu {
             pc: START_ADDRES,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_HEIGHT * SCREEN_WIDTH],
+            keys: [false; NUM_KEYS],
             v_regs: [0; NUM_REGS],
             i_reg: 0,
             sp: 0,
@@ -98,7 +103,7 @@ impl Emu {
                 self.push(self.pc);
                 self.pc = nnn;
             }
-            (3, _, _, _) => { 
+            (3, _, _, _) => {
                 // http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3xkk
                 let x = digit2 as usize;
                 let nn = (op & 0xFF) as u8;
@@ -111,7 +116,7 @@ impl Emu {
                 let x = digit2 as usize;
                 let nn = (op & 0xFF) as u8;
                 if self.v_regs[x] != nn {
-                    self.pc +=2;
+                    self.pc += 2;
                 }
             }
             (5, _, _, 0) => {
@@ -134,7 +139,7 @@ impl Emu {
                 // Adds the value nn to the value of register Vx, then stores the result in Vx.
                 let x = digit2 as usize;
                 let nn = (op & 0xFF) as u8;
-                self.v_regs[x] = self.v_regs[x].wrapping_add(nn); // May overflow, dont't panic. Use wrapping add instead.               
+                self.v_regs[x] = self.v_regs[x].wrapping_add(nn); // May overflow, dont't panic. Use wrapping add instead.
             }
             (8, _, _, 0) => {
                 // Set Vx = Vy.
@@ -178,8 +183,8 @@ impl Emu {
                 let y = digit3 as usize;
 
                 // Set carry 1 if overflow.
-                let (new_vx, carry) = self.v_regs[x].overflowing_add(self.v_regs[y]); 
-                let new_vf = if carry {1}  else {0};
+                let (new_vx, carry) = self.v_regs[x].overflowing_add(self.v_regs[y]);
+                let new_vf = if carry { 1 } else { 0 };
 
                 self.v_regs[x] = new_vx;
                 self.v_regs[0xF] = new_vf;
@@ -188,7 +193,7 @@ impl Emu {
                 let x = digit2 as usize;
                 let y = digit3 as usize;
                 let (new_vx, borrow) = self.v_regs[x].overflowing_sub(self.v_regs[y]);
-                let new_vf = if borrow {0} else {1};
+                let new_vf = if borrow { 0 } else { 1 };
 
                 self.v_regs[x] = new_vx;
                 self.v_regs[0xF] = new_vf;
@@ -199,6 +204,123 @@ impl Emu {
                 self.v_regs[x] >> 1;
                 self.v_regs[0xF] = lsb;
             }
+            (8, _, _, _) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                let (new_vx, borrow) = self.v_regs[y].overflowing_sub(self.v_regs[x]);
+                let new_vf = if borrow { 0 } else { 1 };
+
+                self.v_regs[x] = new_vx;
+                self.v_regs[0xF] = new_vf;
+            }
+            (8, _, _, 0xE) => {
+                let x = digit2 as usize;
+                let msb = (self.v_regs[x] >> 7) & 1;
+                self.v_regs[x] <<= 1;
+                self.v_regs[0xE] = msb;
+            }
+            (9, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+
+                if self.v_regs[x] != self.v_regs[y] {
+                    self.pc += 2;
+                }
+            }
+            (0xA, _, _, _) => {
+                let nnn = op & 0xFFF;
+                self.i_reg = nnn;
+            }
+            (0xB, _, _, _) => {
+                let nnn = op & 0xFFF;
+                self.pc = (self.v_regs[0] as u16) + nnn;
+            }
+            (0xC, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0xFF) as u8;
+                let rng: u8 = random();
+                self.v_regs[x] = rng & nn;
+            }
+            (0xD, _, _, _) => {
+                // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+                let x_coord = self.v_regs[digit2 as usize] as u16;
+                let y_coord = self.v_regs[digit3 as usize] as u16;
+                let num_rows = digit4 as u16;
+                let mut flipped = false;
+
+                for y_line in 0..num_rows {
+                    let addr = self.i_reg + y_line as u16;
+                    let pixels = self.ram[addr as usize];
+                    for x_line in 0..8 {
+                        if (pixels & (0b1000_0000 >> x_line)) !=0 {
+                            let x = (x_coord + x_line) as usize % SCREEN_HEIGHT;
+                            let y = (y_coord + y_line) as usize % SCREEN_HEIGHT;
+
+                            let idx = x + SCREEN_WIDTH * y;
+                            flipped |= self.screen[idx];
+                            self.screen[idx] ^= true;
+                        }
+                    }
+                }
+                if flipped {
+                    self.v_regs[0xF] = 1;
+                } else {
+                    self.v_regs[0xF] = 0;
+                }
+            }
+            (0xE, _, 9, 0xE) => {
+                let x = digit2 as usize;
+                let vx = self.v_regs[x];
+                let key = self.keys[vx as usize];
+                if key {
+                    self.pc += 2;
+                }
+            }
+            (0xE, _, 0xA, 1) => {
+                let x = digit2 as usize;
+                let vx = self.v_regs[x];
+                let key = self.keys[vx as usize];
+                if !key {
+                    self.pc += 2;
+                }
+            }
+            (0xF, _, 0, 7) => {
+                let x = digit2 as usize;
+                self.v_regs[x] = self.dt;
+            }
+            (0xF, _, 0, 0xA) => {
+                let x = digit2 as usize;
+                let mut pressed = false;
+
+                for i in 0..self.keys.len() {
+                    if self.keys[i] {
+                        self.v_regs[x] = i as u8;
+                        pressed = true;
+                        break;
+                    }
+                }
+                if !pressed {
+                    // Redo operator.
+                    self.pc -= 2;
+                }
+            }
+            (0xF, _, 1, 5) => {
+                let x = digit2 as usize;
+                self.dt = self.v_regs[x];
+            }
+            (0xF, _, 1, 8) => {
+                let x = digit2 as usize;
+                self.st = self.v_regs[x]
+            }
+            (0xF, _, 1, 0xE) => {
+                //Set I = I + Vx.
+                // The values of I and Vx are added, and the results are stored in I.
+                let x = digit2 as usize;
+                let vx = self.v_regs[x] as u16;
+                self.i_reg = self.i_reg.wrapping_add(vx);
+            }
+            
+
 
         }
     }
